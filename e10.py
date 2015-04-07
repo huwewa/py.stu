e17081fe7e2ee4128b08d56311a8c35a07871316
wywon <huwewa@gmail.com>
2015-04-07 18:30:33 +0800

    init

---
+++ e10.py
@@ -0,0 +1,11 @@
+days = "Mon Tue Web Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "days: ", days
+print "months: ", months
+print """
+sdfa
+sdfsd
+fdfd
+sdfasd
+"""
---
+++ e9.py
@@ -0,0 +1,6 @@
+formatter = "%s %s %s %s"
+
+print formatter % (1,2,3,4)
+print formatter % (True,False,False,False)
+print formatter % ("one","two","three","four")
+print formatter % ("我","们","是","雷")
